5e3af99bf4d7ff48affc2190d8c797a4f6ab805e
sandhya425 <rajputsandhya425@gmail.com>
2025-01-13 16:19:37 +0530

    cpp files

---
+++ second.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a=4,b=3,c=9;
+    cout<<"a="<<a<<"b="<<b<<endl;
+    cout<<c<<endl;
+    cout<<sizeof(a)<<sizeof(b);
+    return 0;}
---
+++ sum.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main(){
+   // int a=3;
+   // int b=6;
+    //cout<<"sum of\t"<<a << " and " << b <<" "<<a+b ;
+    //return 0;
+    //char ch ='\0';//char ch----------->no output
+
+     char ch='a';
+     char ch1='b';//char ch='\0'----------->prefer
+    //cout<<ch;
+   // cout<<sizeof(ch1)<<endl;
+   // cout<<sizeof(ch)<<endl;
+  // cout<<int(ch)<<endl;<--ascii value k liye -->
+    //cout<<int(ch1)<<endl;
+   // cout<<int(ch)+int(ch1)<<endl;
+
+
+
+
+
+
+    return 0;
+}
