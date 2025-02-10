d0683d39d188edb5674278b5fffa3672e70c727c
sandhya425 <rajputsandhya425@gmail.com>
2025-02-10 16:57:10 +0530

    ques

--- arithematic.cpp
+++ arithematic.cpp
@@ -1,114 +1,109 @@
-#include<iostream>
-#include<string.h>
+#include <iostream>
+#include <string.h>
 using namespace std;
-int main(){
-   // int num1,num2,num3;
-   // cout<<"enter two number";
-   // cin>>num1>>num2>>num3;
-   // cout<<num1*num2*num3<<endl;
-   //cout<<num1+num2;
-   //int num1,num2;
-   //cin>>num1>>num2;
+int main()
+{
+  // int num1,num2,num3;
+  // cout<<"enter two number";
+  // cin>>num1>>num2>>num3;
+  // cout<<num1*num2*num3<<endl;
+  // cout<<num1+num2;
+  // int num1,num2;
+  // cin>>num1>>num2;
   // cout<<num1/num2;
   // cout<<num1%num2;
- //// int n1,r=12;
-  //cout<<"area of circle is"<<22/7*r*r;
-  //int n,n2;
-  //cin>>n>>n2;
-  //cout<<n-n2;
-  //int n;
-//   cin>>n;
-//   if(n%2)
-//   cout<<"number is odd";
-//   else
-//   cout<<"number is even";
-// int n;
-// cin>>n;
-// cout<<"the square of number is"<<n*n;
-// int m,n,o,p,q;
-// float avg;
-// cin>>m>>n>>o>>p>>q;
-// avg=m+n+o+p+q/5.0;
-// cout<<"average of five number"<<avg;
-// int p,r,t;
-// int sm;
-// cin>>p>>r>>t;
-// sm=p*r*t/100;
-// cout<<"simple interest"<<sm;
-// int a,b,c,d,e,f;
-// cin>>a>>b>>c>>d>>e;
-// f=a+b+c+d+e/100*1/100;
-// cout<<"marks"<<f;
-// bool com,com2;
-// bool com2;
-// com=2>6;
-// com2=30==67;
-// cout<<com<<"and"<<com2;
-// cout<<sizeof(bool
-// com=10.43;
-// com2=56.00;
-// cout<<com/com2;
+  //// int n1,r=12;
+  // cout<<"area of circle is"<<22/7*r*r;
+  // int n,n2;
+  // cin>>n>>n2;
+  // cout<<n-n2;
+  // int n;
+  //   cin>>n;
+  //   if(n%2)
+  //   cout<<"number is odd";
+  //   else
+  //   cout<<"number is even";
+  // int n;
+  // cin>>n;
+  // cout<<"the square of number is"<<n*n;
+  // int m,n,o,p,q;
+  // float avg;
+  // cin>>m>>n>>o>>p>>q;
+  // avg=m+n+o+p+q/5.0;
+  // cout<<"average of five number"<<avg;
+  // int p,r,t;
+  // int sm;
+  // cin>>p>>r>>t;
+  // sm=p*r*t/100;
+  // cout<<"simple interest"<<sm;
+  // int a,b,c,d,e,f;
+  // cin>>a>>b>>c>>d>>e;
+  // f=a+b+c+d+e/100*1/100;
+  // cout<<"marks"<<f;
+  // bool com,com2;
+  // bool com2;
+  // com=2>6;
+  // com2=30==67;
+  // cout<<com<<"and"<<com2;
+  // cout<<sizeof(bool
+  // com=10.43;
+  // com2=56.00;
+  // cout<<com/com2;
 
+  //  bool log,log1;//unly use of comparison opretor bool
+  //  log=56&&54;
+  //  log1=6||7;
+  //  cout<<log<<"\n"<<log1;
 
-//  bool log,log1;//unly use of comparison opretor bool 
-//  log=56&&54;
-//  log1=6||7;
-//  cout<<log<<"\n"<<log1;
+  // int a,discount;
+  // cin>>a;
+  // discount=(a*10/100)-a;
+  // cout<<"the price is"<<discount;
+  // int distance=240;
+  // int time=4;
+  // int speed;
+  // speed=distance/time;
+  // cout<<speed<<"km/hr";
+  // cout<<"speed is"<<(distance*1000)/(hour*60*60)<<"m/s";
 
+  // que:wap to convert temperature from celsius to fahrenheight using formula f=9/5*c+32
+  // float f,c;
+  // cout<<"enter temperature in degree celsius";
+  // cin>>c;
+  // f=9/5*c+32;
+  // cout<<"temperature in fahrenheight"<<f;
 
-// int a,discount;
-// cin>>a;
-// discount=(a*10/100)-a;
-// cout<<"the price is"<<discount;
-// int distance=240;
-// int time=4;
-// int speed;
-// speed=distance/time;
-// cout<<speed<<"km/hr";
-// cout<<"speed is"<<(distance*1000)/(hour*60*60)<<"m/s";
+  // wap to calculate area of circle and perimeter of circle where r=7;pi=22/7;
+  // int r=7;
+  // int pi=22/7;
+  // int area,perimeter;
+  // area=22/7*r*r;
+  // perimeter=2*22/7*r;
+  // cout<<area<<"\n"<<perimeter;
 
+  // wap to cal. the total markes and avg,percentage of student in agiven 5 subj.
+  // int a,b,c,d,e;
+  // int total,avg,percentage;
+  // cin>>a>>b>>c>>e>>d;
+  // total=a+b+c+e+d;
+  // avg=a+b+c+d+e/5;
+  // percentage=total*100;
+  // cout<<total<<"\n";
+  // cout<<avg<<"\n";
+  // cout<<percentage<<"percentage";
 
-// que:wap to convert temperature from celsius to fahrenheight using formula f=9/5*c+32
-// float f,c;
-// cout<<"enter temperature in degree celsius";
-// cin>>c;
-// f=9/5*c+32;
-// cout<<"temperature in fahrenheight"<<f;
+  // wap to check if the division of two num. rezults in an integer or a float.
 
-// wap to calculate area of circle and perimeter of circle where r=7;pi=22/7;
-// int r=7;
-// int pi=22/7;
-// int area,perimeter;
-// area=22/7*r*r;
-// perimeter=2*22/7*r;
-// cout<<area<<"\n"<<perimeter;
+  // #handle case where the user attemt to divide by 0 and print appropriate error massage.
+  // int numerator,denomenetor;
+  // cin>> numerator>>denomenetor;
+  // if(denomenetor==0){
+  //   cout<<"denometer should not be 0 ";
+  // }
+  // else{
+  //   cout<<" the division of the numerator and denomenetor output is ans="<<numerator /denomenetor ;
 
-
-// wap to cal. the total markes and avg,percentage of student in agiven 5 subj.
-// int a,b,c,d,e;
-// int total,avg,percentage;
-// cin>>a>>b>>c>>e>>d;
-// total=a+b+c+e+d;
-// avg=a+b+c+d+e/5;
-// percentage=total*100;
-// cout<<total<<"\n";
-// cout<<avg<<"\n";
-// cout<<percentage<<"percentage";
-
-
-// wap to check if the division of two num. rezults in an integer or a float.
-
-// #handle case where the user attemt to divide by 0 and print appropriate error massage.
-// int numerator,denomenetor;
-// cin>> numerator>>denomenetor;
-// if(denomenetor==0){
-//   cout<<"denometer should not be 0 ";
-// }
-// else{
-//   cout<<" the division of the numerator and denomenetor output is ans="<<numerator /denomenetor ;
-
-// }
-   
+  // }
 
   //  int n;
   //  cin>>n;
@@ -133,51 +128,74 @@ int main(){
   // else
   //   cout<<"the student is fail";
 
-//   int a;
-//    cin>>a;
-//    if(a>=18)
-//     cout<<"eligible for vote";
-//  else
-//      cout<<"not eligible for vote";
+  //   int a;
+  //    cin>>a;
+  //    if(a>=18)
+  //     cout<<"eligible for vote";
+  //  else
+  //      cout<<"not eligible for vote";
 
-// int x,y;
-//    cin>>x>>y;
-//    if(x>y)
-//     cout<<"this producd is high lable";
-//    else
-//      cout<<"this is cheaper product";
+  // int x,y;
+  //    cin>>x>>y;
+  //    if(x>y)
+  //     cout<<"this producd is high lable";
+  //    else
+  //      cout<<"this is cheaper product";
 
+  // string m="aarti";
+  // string n="aarti";
+  // if(m==n){
+  //   cout<<"the strings are  equal";
+  //   }
+  //  else
+  //     cout<<"strings are not equal";
 
-// string m="aarti";
-// string n="aarti";
-// if(m==n){
-//   cout<<"the strings are  equal";
+  // int a,b,c;
+  // cin>>a>>b>>c;
+  // if(a>b &&a>c)
+  //   cout<<"a is greater";
+  // else if(b>a&&b>c)
+  //  cout<<"b is greater";
+  //  else
+  // //   cout<<"c is greater";
+
+  // int n;
+  // cin>>n;
+  // if(n>=10&&n<=50)
+  // cout<<"the number is between 10 and 50";
+  // else
+  // cout<<"the number is not lie 10 and 50";
+
+  // int year;
+  // cin>>year;
+  // if(year%400==0)
+  //   cout<<"this is leap year";
+  //   else if(year%4==0 &&year%100!=0)
+  //     cout<<"this is leap year";
+  //   else
+  //     cout<<"not leap year";
+
+  // for(int i=10;i>=0;i--){
+  //   if(i>=1)
+  //   cout<<i<<"\n";
+  //   else
+  //   cout<<"liftofff";
+  //   return 0;
+  // }
+
+// #wap that keeps asking the user to input a number.add all the numbers togheter and stop asking when the user enter 0. finally print the total sum.
+// int store=0,inp;
+// while(1){
+    //  cout<<"enter 0 for exit\n";
+//   cout<<"enter the number";
+//   cin>>inp;
+//   if(inp!=0){
+//   store=store+inp;
 //   }
-//  else
-//     cout<<"strings are not equal";
-
-// int a,b,c;
-// cin>>a>>b>>c;
-// if(a>b &&a>c)
-//   cout<<"a is greater";
-// else if(b>a&&b>c)
-//  cout<<"b is greater";
-//  else
-// //   cout<<"c is greater";
-
-// int n;
-// cin>>n;
-// if(n>=10&&n<=50)
-// cout<<"the number is between 10 and 50";
-// else
-// cout<<"the number is not lie 10 and 50";
-// int year;
-// cin>>year;
-// if(year%400==0)
-//   cout<<"this is leap year";
-//   else if(year%4==0 &&year%100!=0)
-//     cout<<"this is leap year";
-//   else 
-//     cout<<"not leap year";
-//     return 0;
+//   else{
+//     cout<<"sum of previous enter value is="<<store;
+//     break;
+//   }
+// }
+  return 0;
 }
