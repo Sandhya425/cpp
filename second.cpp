95aa5ac1b9321786ca2211c293dae46b72ff7ed5
sandhya425 <rajputsandhya425@gmail.com>
2025-04-02 23:49:00 +0530

    power sqrt

--- second.cpp
+++ second.cpp
@@ -1,8 +1,18 @@
 #include<iostream>
+#include<math.h>
 using namespace std;
 int main(){
-    int a=4,b=3,c=9;
-    cout<<"a="<<a<<"b="<<b<<endl;
-    cout<<c<<endl;
-    cout<<sizeof(a)<<sizeof(b);
+    // int a=4,b=3,c=9;
+    // cout<<"a="<<a<<"b="<<b<<endl;
+    // cout<<c<<endl;
+    // cout<<sizeof(a)<<sizeof(b);
+
+    // int c=49;
+    // int y=sqrt(c);
+    // cout<<y;
+
+    // int a,b,c;
+    // cin>>a>>b;
+    // c=pow(a,b);
+    // cout<<c;
     return 0;}
