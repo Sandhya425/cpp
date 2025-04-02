90f8dab4771bfa246671f5772352ba3cceca0ce4
sandhya425 <rajputsandhya425@gmail.com>
2025-04-02 14:27:10 +0530

    pattern

--- arithematic.cpp
+++ arithematic.cpp
@@ -623,15 +623,16 @@ int main()
 //   cout<<"\n";
 // }
 
-
-for(int i=1;i<=10;i++){
-  for(int j=1;j<=10;j++){
-    if(i%2==0)
-      cout<<"*";
-      else
-      cout<<" ";
-  }
-  cout<<"\n";
-}
+// int n;
+// cin>>n;
+// for(int i=1;i<=n;i++){
+//   for(int j=1;j<=n;j++){
+//     if(i==1||i==n||j==1||j==n)
+//       cout<<"*";
+//       else
+//       cout<<" ";
+//   }
+//   cout<<"\n";
+// }
   return 0;
 }
