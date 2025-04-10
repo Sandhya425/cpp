b7e090ed4101e7d4fa3d4efbd4266611f6cd6c9f
sandhya425 <rajputsandhya425@gmail.com>
2025-04-10 10:30:11 +0530

    ascending

--- NCR.cpp
+++ NCR.cpp
@@ -1,25 +1,39 @@
 #include<iostream>
 using namespace std;
-void yrr();
-int main(){
+// void yrr();
+// int main(){
 
-    yrr();
-    return 0;
- }
-void yrr(){
-int arr[5]={1,2,3,4,5};
-int len=sizeof(arr);
-int flen=len/4;
-int n,dec=0;
-cin>>n;
-for(int i=0;i<flen;i++){
-if(n==arr[i]){
-    dec=1;//dec++;
-}
-}
-if(dec==1)
- cout<<"found";
- else
- cout<<"not found";
+//     yrr();
+//     return 0;
+//  }
+// void yrr(){
+// int arr[5]={1,2,3,4,5};
+// int len=sizeof(arr);
+// int flen=len/4;
+// int n,dec=0;
+// cin>>n;
+// for(int i=0;i<flen;i++){
+// if(n==arr[i]){
+//     dec=1;//dec++;
+// }
+// }
+// if(dec==1)
+//  cout<<"found";
+//  else
+//  cout<<"not found";
 
-}
+// }
+// void factor(int);
+// int main(){
+//     int n,r;
+//     cin>>n>>r;
+//     factor(n)/(factor(r)factor(n-r));
+//     return 0;
+// }
+// void factor(int n){
+//     // int fact=1;
+//     for(int i=1;i<=n;i++){
+//         fact =fact*i;
+//     }
+//     cout<<fact;
+// }
--- reverse.cpp
+++ reverse.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
-void fun();
-int main(){
+
+
     // int arr[]={1,3,4,5,6};
     // int y[5];
     // int sum=0,sumodd=0;
@@ -23,13 +23,39 @@ int main(){
 //     cout<<fact;
 // }
 
-int arr[5],arr1[5];
-int i,n;
-cin>>n;
-for(i=0;i<n;i++){
-    cin>>arr[i];
-    cout<<arr[i];
-}
-for(i=0;i<n;i++){
-  arr1[]
-}
+// int arr[5],arr1[5];
+// int i,n;
+// cin>>n;
+// for(i=0;i<n;i++){
+//     cin>>arr[i];
+//     cout<<arr[i];
+// }
+// for(i=0;i<n;i++){
+//   arr1[i]=arr[i];
+//   cout<<arr1[i];
+// }
+
+//ascending oreder.
+// void s(int arr[],int len){
+//   for(int i=0;i<len;i++){
+//     for(int j=0;j<len-1;j++){
+//       if(arr[i]<arr[j]){
+//         int temp=arr[i];
+//         arr[i]=arr[j];
+//         arr[j]=temp;
+//       }
+//     }
+//   }
+// }
+// void output(int arr[],int len){
+//   for(int i=0;i<len;i++){
+//     cout<<arr[i]<<" ";
+//   }
+// }
+// int main(){
+// int arr[]={34,56,2,7,9,5,88,3};
+// int len=sizeof(arr)/4;
+// s(arr,len);
+// output(arr,len);
+// return 0;
+// }
