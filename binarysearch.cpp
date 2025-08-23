a103759261e4633edc7c3d42801770046996ada2
sandhya425 <rajputsandhya425@gmail.com>
2025-08-23 11:25:52 +0530

    sorting

---
+++ binarysearch.cpp
@@ -0,0 +1,83 @@
+// increasing order 1 2 3 4 5.
+//#include<iostream>
+// using namespace std;
+// int main(){
+//     int arr[1000];
+//     int n;
+//     cout<<"enter sixe";
+//     cin>>n;
+//     cout<<"enter elemnts";
+//     for(int i=0;i<n;i++){
+//         cin>>arr[i];
+//     }
+//     int start=0,end=n-1,key;
+//     cout<<"enter key";
+//     cin>>key;
+//     while(start<=end){
+//         int mid=start+(end-start)/2;
+//         if(arr[mid]==key){
+//             cout<<mid;
+//             break;
+//         }
+//         else if(arr[mid]<key){
+//             start=mid+1;//right side
+//         }
+//         else{
+//           end=mid-1;
+//         }
+//     }
+//     return 0;
+// }
+
+
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int arr[1000];
+//     int n;
+//     cout<<"enter sixe";
+//     cin>>n;
+//     cout<<"enter elemnts";
+//     for(int i=0;i<n;i++){
+//         cin>>arr[i];
+//     }
+
+// }
+
+
+//decreasing order 30,20,10,4,6;
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int arr[1000];
+//     int n;
+//     cout<<"enter sixe";
+//     cin>>n;
+//     cout<<"enter elemnts";
+//     for(int i=0;i<n;i++){
+//         cin>>arr[i];
+//     }
+//     int start=0,end=n-1,key;
+//     cout<<"enter key";
+//     cin>>key;
+//     int mid=start+(end-start)/2;
+//     while(start<=end){
+//         if(arr[mid]==key){
+//             cout<<mid;
+//             break;
+//         }
+//         else if(arr[mid]<key){
+//             end=mid-1;// side
+//         }
+//         else{
+//           start=mid+1;
+//         }
+//           mid=start+(end-start)/2;
+//     }
+//     return 0;
+// }
+
+
+
+
